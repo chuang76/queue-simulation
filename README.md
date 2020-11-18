acb29b4cb92d4a7fc58a671adfb34dc8d72a6b43
chuang76 <ychin.chuang@gmail.com>
2020-11-18 21:56:52 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# queue-simulation
+# Queue simulation
+
+The goal of this project is to simulate M/M/S/S queuing model [(Erlang B)](https://en.wikipedia.org/wiki/Erlang_(unit)#Erlang_B_formula) and construct the blocking probability table. 
+
+
+
+## Environment
+
+- Python 3.6
+
+
+
+## Usage
+
+```
+$ python src.py
+```
+
+
+
+## Policy
+
+- Generate 100000 customers, both arrival time and service time follow the [Poisson distribution](https://en.wikipedia.org/wiki/Poisson_distribution)
+- Blocking probability = number of blocking / number of arrival
+
+
+
+## Result
+
+| Erlang ($\lambda / \mu$) | blocking prob. (S = 1) | blocking prob. (S = 5) | blocking prob. (S = 10) |
+| ------------------------ | ---------------------- | ---------------------- | ----------------------- |
+| 1.0000                   | 0.5012                 | 0.0031                 | 0.0000                  |
+| 0.2500                   | 0.1998                 | 0.0000                 | 0.0000                  |
+| 0.0625                   | 0.0590                 | 0.0000                 | 0.0000                  |
+| 0.0156                   | 0.0154                 | 0.0000                 | 0.0000                  |
+| 0.0039                   | 0.0039                 | 0.0000                 | 0.0000                  |
+| 0.0010                   | 0.0010                 | 0.0000                 | 0.0000                  |
+| 10.0000                  | 0.9093                 | 0.5640                 | 0.2150                  |
+| 2.5000                   | 0.7136                 | 0.0696                 | 0.0002                  |
+| 0.6250                   | 0.3846                 | 0.0004                 | 0.0000                  |
+| 0.1562                   | 0.1352                 | 0.0000                 | 0.0000                  |
+| 0.0391                   | 0.0373                 | 0.0000                 | 0.0000                  |
+| 0.0098                   | 0.0096                 | 0.0000                 | 0.0000                  |
+| 100.0000                 | 0.9900                 | 0.9505                 | 0.9006                  |
+| 25.0000                  | 0.9617                 | 0.8084                 | 0.6218                  |
+| 6.2500                   | 0.8615                 | 0.3775                 | 0.0505                  |
+| 1.5625                   | 0.6096                 | 0.0164                 | 0.0000                  |
+| 0.3906                   | 0.2812                 | 0.0001                 | 0.0000                  |
+| 0.0977                   | 0.0891                 | 0.0000                 | 0.0000                  |
+| 1000.0000                | 0.9990                 | 0.9950                 | 0.9897                  |
+| 250.0000                 | 0.9960                 | 0.9799                 | 0.9597                  |
+| 62.5000                  | 0.9844                 | 0.9212                 | 0.8431                  |
+| 15.6250                  | 0.9397                 | 0.7045                 | 0.4279                  |
+| 3.9062                   | 0.7963                 | 0.1914                 | 0.0047                  |
+| 0.9766                   | 0.4935                 | 0.0028                 | 0.0000                  |
+
--- src.py
+++ src.py
@@ -9,7 +9,7 @@ class Customer:
         self.id = id 
         self.arrival_time = arrival_time
         self.service_time = poisson(mu)         
-        self.status = status
+        self.status = status            # 0: init, 1: service, 2: finish
 
 Q = 0 
 S = [1, 5, 10]
@@ -17,67 +17,80 @@ lambds = [0.01, 0.1, 1, 10]
 mus = [0.01, 0.04, 0.16, 0.64, 2.56, 10.24]
 runs, N = 10, 100000
 
-# print('\nQ = 0')
 for s in S:
     print('\n(S = %d) Erlang | block prob' %(s))
     print('-------------------------------')
 
     for lambd in lambds:
         for mu in mus:
-            block_prob = 0                                  # blocking probability 
+            block_prob = 0                                  # blocking prob  
             for run in range(runs):
                 
-                system = []                                 # initialize  
-                space_server, num_wait = s, 0
-                arrival, block = 0, 0      
+                system, table = [], []                      # initialize  
+                capacity, num_wait = s, 0
+                arrival, block = 0, 0  
+                system_time = 0;   
 
-                # 1st customer 
-                system.append(Customer(0, poisson(lambd), mu, True))
-                arrival += 1
-                space_server -= 1 
+                for i in range(0, N):
 
-                for i in range(1, N):
-                    
-                    depart = 0                               # number of departure
-                    next_arrival = poisson(lambd)
-
-                    # departure 
-                    system_cp = copy.copy(system)
-                    for c in system:
-                        if c.status == True:                 # service
-                            t = c.service_time - next_arrival
-                            if t <= 0:                       # should leave 
-                                system_cp.remove(c)          # remove 
-                                space_server += 1            # leave an empty space 
-                            else:
-                                c.service_time = t           # still process, update the service time 
-                    system = system_cp                       # update the system 
-                    
-                    # queue -> server 
-                    for c in system:                         
-                        if space_server > 0:               
-                            if c.status == False:    
-                                c.status = True              # start servicing   
-                                num_wait -= 1                
-                                space_server -= 1 
-                        else:
-                            break 
-
-                    # current customer 
+                    # generate the customer 
+                    customer = Customer(arrival, poisson(lambd), mu, 0)
                     arrival += 1
-                    if space_server > 0:                     # there are still some empty spaces in the server, service directly 
-                        c = Customer(i, next_arrival, mu, True)
-                        system.append(c)
-                        space_server -= 1
-                    elif (num_wait < Q * s):                 # queue < Q * s
-                        c = Customer(i, next_arrival, mu, False)
-                        system.append(c)
-                        num_wait += 1 
-                    else:
+
+                    if (capacity == 0):                      # no space -> block
                         block += 1
+                    else:
+                        if ((capacity - num_wait) > 0):
+                            customer.status = 1             # start servicing
+                            capacity -= 1 
+                        else:
+                            customer.status = 0 
+                            num_wait += 1
+                            capacity -= 1
+                        system.append(customer)
+
+                    # next 
+                    next_arrival = poisson(lambd)
+                    system_time += next_arrival
+                    depart = 0 
+                    system_cp = copy.copy(system)
+
+                    # check the customers in the system 
+                    for customer in system:
+                        if customer.status == True:
+                            t = customer.service_time - next_arrival
+                            if t <= 0:
+                                customer.status = 2             # finish 
+                                capacity += 1
+                                depart += 1 
+                                table.append(-t) 
+                                system_cp.remove(customer)
+                            else:
+                                customer.service_time = t       # still service 
+
+                        # queue -> system 
+                        else:
+                            if depart > 0:    
+                                num_wait -= 1                 
+                                depart -= 1
+                                customer.status = 1 
+                                customer.service_time -= table[0]; 
+                                if (customer.service_time <= 0):
+                                    customer.status = 2         # finish 
+                                    depart += 1
+                                    capacity += 1
+                                    table.remove(table[0])
+                                    table.append(-customer.service_time)
+                                    system_cp.remove(customer)
+                                else:
+                                    table.remove(table[0])
+                            else:
+                                break 
+
+                    system = system_cp                          # update the system 
+                    table = []
 
                 block_prob += float(block / arrival)
-
             block_prob = float(block_prob / runs)
             print("    %10.4f | %.4f" % (float(lambd / mu), block_prob))
 
